f6a93cff5a148adda586ee23cdcd2d08ca01385c
acasti2048 <alistaircastillo@btinternet.com>
2023-12-29 17:51:26 +0000

    Started on function

--- function.py
+++ function.py
@@ -1,4 +1,7 @@
-import os
-from library import input as inp
+import datetime as dt
 
-#Got an urgent request 
+new_month = dt.date.today().replace(day=28) + dt.timedelta (days = 4)
+eo_month = new_month.replace(day=1)-dt.timedelta(days = 1)
+prior_eo_month = eo_month.replace(day=1)-dt.timedelta(days = 1)
+
+print(eo_month, prior_eo_month)
